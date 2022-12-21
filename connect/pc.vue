c60780fff9bb584d3ade2c148deab32871522c25
hhl <18302085349@jin10.co>
2022-12-21 16:16:54 +0800

    ？？？

--- connect/pc.vue
+++ connect/pc.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="pc">pc页面
-<<<<<<< HEAD
-    <div class="third">第三阶段的文字</div>
-=======
     <div class="third">第三阶段的文字（非要改一下吗？）</div>
->>>>>>> parent of fbd0059 (feat：选择第二)
     <img src="./images/top-bg.png" alt="">
     <div class="bottom-btn">
       继续开发我的第三阶段页面
