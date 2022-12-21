fbd0059da6a379e49207f8d6911e385de685804c
hhl <18302085349@jin10.co>
2022-12-21 15:45:22 +0800

    feat：选择第二

--- connect/pc.vue
+++ connect/pc.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="pc">pc页面
-    <div class="third">第三阶段的文字（非要改一下吗？）</div>
-    <img src="./images/top-bg.png" alt="">
-    <div class="bottom-btn">
-      继续开发我的第三阶段页面
+    <img src="./images/flash.png" alt="">
+    <div class="second">第二阶段的文字</div>
+    <div class="rule-wrap">
+      <p>1.测试代码</p>
+      <p>2.测试代码</p>
+      <p>3.测试代码</p>
+      <p>4.测试代码</p>
+      <p>5.测试代码</p>
     </div>
   </div>
 </template>
