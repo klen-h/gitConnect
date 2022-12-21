bc8f9274c91554740081864151dc060afea640db
hhl <18302085349@jin10.co>
2022-12-21 14:13:46 +0800

    fix:提交第二阶段完整

--- connect/pc.vue
+++ connect/pc.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="pc">pc页面
-    <img src="./images/top-bg.png" alt="">
+    <img src="./images/flash.png" alt="">
+    <div class="second">第二阶段的文字</div>
+    <div class="rule-wrap">
+      <p>1.测试代码</p>
+      <p>2.测试代码</p>
+      <p>3.测试代码</p>
+      <p>4.测试代码</p>
+      <p>5.测试代码</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: "Pc",
+  data() {
+    return {
+      isEnabled:false,
+    }
+  },
 }
 </script>
 
 <style>
-
+.second {
+  width: 200px;
+  height: 100px;
+  background-color: pink;
+}
 </style>
