05df05da65e4d5d554410ddec2e0d09f93701f3b
hhl <18302085349@jin10.co>
2022-12-21 14:37:25 +0800

    什么鬼

--- connect/pc.vue
+++ connect/pc.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="pc">pc页面
-    <div class="third">第三阶段的文字</div>
-    <img src="./images/top-bg.png" alt="">
-    <div class="bottom-btn">
-      继续开发我的第三阶段页面
+    <img src="./images/flash.png" alt="">
+    <div class="second">第二阶段的文字</div>
+    <div class="rule-wrap">
+      <p>1.测试代码</p>
+      <p>2.测试代码</p>
+      <p>3.测试代码</p>
+      <p>4.测试代码</p>
+      <p>5.测试代码</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: "Pc",
   data() {
     return {
-      test: false,
+      isEnabled:false,
     }
   },
 }
