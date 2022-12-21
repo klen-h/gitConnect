62ea1eed28791f73c06ed2560357a7c5ef496b14
hhl <18302085349@jin10.co>
2022-12-21 15:32:20 +0800

    提交

--- connect/pc.vue
+++ connect/pc.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pc">pc页面
-    <div class="third">第三阶段的文字</div>
+    <div class="third">第三阶段的文字（非要改一下吗？）</div>
     <img src="./images/top-bg.png" alt="">
     <div class="bottom-btn">
       继续开发我的第三阶段页面
